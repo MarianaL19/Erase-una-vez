6a678c98d9fe9fbf4dcb7c049a48986328610782
Oliverso2K <oliverisrael.quinterogarcia@gmail.com>
2022-11-08 15:33:14 -0600

    add: Usuario
    Implementé el singleton de usuario.

---
+++ Erase-una-vez/Assets/Scripts/Usuario.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Usuario : MonoBehaviour
+{
+    [SerializeField] private int NoEstrellas, NivelActual;
+
+    // Instancia de la clase
+    public static Usuario Instancia { get; private set; }
+
+    private void Awake ()
+    {
+        if (Instancia != null && Instancia != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instancia = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
