3beb512fcf48198c551bc963a6f63da3dc45db4a
Osvaldo González <gonzalezascencioosvaldo@outlook.com>
2022-11-27 16:12:38 -0600

    update: ControlPantallaVictoria
    Actualicé el script para poder obtener y guardar las estrellas, tiene las validaciones para no poder farmear las estrellas que ya obtuviste en un nivel.

--- Erase-una-vez/Assets/Scripts/ControlPantallaVictoria.cs
+++ Erase-una-vez/Assets/Scripts/ControlPantallaVictoria.cs
@@ -14,12 +14,42 @@ public class ControlPantallaVictoria : MonoBehaviour
     [SerializeField] private GameObject panelVictoria;
     [SerializeField] private Escritura escritura;
 
+    //Variable para identificar nivel
+    public int numNivel;
+    public bool audio;
+    public int estrellaPrecision;
+    public int estrellaTiempo;
+    public int estrellasTotales;
+    public int estrellaTerminado;
+
     public void Start()
     {
     }
     public void NivelFinalizado()
     {
-        bool audio = false;
+        //Recibir de que nivel viene
+        numNivel = PlayerPrefs.GetInt("nivelActual");
+
+        //Verificar audio o normal
+        audio = PlayerPrefs.GetInt("varianteAudio" + numNivel) == 1 ? true : false;
+
+        //Recuperamos las estrellas del nivel
+        estrellaPrecision = PlayerPrefs.GetInt("estrellaPrecision" + numNivel); // Recupero si la estrella de precisión ya fue desbloqueada    
+        estrellaTiempo = PlayerPrefs.GetInt("estrellaTiempo" + numNivel); // Recupero si la estrella de tiempo ya fue desbloqueada
+        estrellaTerminado = PlayerPrefs.GetInt("estrellaTerminado" + numNivel); // Recupero si la estrella de terminar ya fue desbloqueada
+
+        //Logs para ver si las estrellas ya estaban desde antes
+        Debug.Log("Estrella Precision Antes: " + estrellaPrecision);
+        Debug.Log("Estrella Tiempo Antes: " + estrellaTiempo);
+        Debug.Log("Estrella Terminado Antes: " + estrellaTerminado);
+
+        estrellasTotales = PlayerPrefs.GetInt("noEstrellas"); // Recupero la cantidad total de estrellas
+        Debug.Log("Estrellas Totales Antes:" + estrellasTotales); // Muestro la cantidad total de estrellas antes del nivel
+        
+
+        //Guardamos la estrella por completar el nivel
+        PlayerPrefs.SetInt("estrellaTerminado" + numNivel, 1);
+
         int estrellasMostrar= 1;
         //PlayerPrefs.GetInt("varianteAudio") == 1;
         if(audio)
@@ -41,14 +71,50 @@ public class ControlPantallaVictoria : MonoBehaviour
 
             int tiempo = escritura.getTiempo();
             int aciertos = escritura.getAciertos();
-            if (tiempo < 50)
+         
+            if (tiempo < 10){
+
+                if(estrellaTiempo == 0){
+                    estrellasTotales++;
+                }
+
                 estrellasMostrar++;
-            if (aciertos > 50)
-                estrellasMostrar++;
-            if(estrellasMostrar>1)
+                PlayerPrefs.SetInt("estrellaTiempo" + numNivel, 1);
+                estrellaTiempo = PlayerPrefs.GetInt("estrellaTiempo" + numNivel); // Marco la estrella de tiempo como desbloqueada
+            }
+                
+            if (aciertos > 500) {
+
+                if(estrellaPrecision == 0){
+                    estrellasTotales++;
+                }
+
+                estrellasMostrar++; 
+                PlayerPrefs.SetInt("estrellaPrecision" + numNivel, 1);
+                estrellaPrecision = PlayerPrefs.GetInt("estrellaPrecision" + numNivel); // Marco la estrella de precision como desbloqueada    
+            }
+
+            if (estrellaTerminado == 0) {
+                estrellasTotales++; 
+            }
+                
+            if(estrellasMostrar>1){
                 estrella2.SetActive(true);
-            if(estrellasMostrar>2)
+            }
+                
+            if(estrellasMostrar>2){
                 estrella3.SetActive(true);
+            }
+
+            //Logs para ver si las estrellas son otorgadas
+            Debug.Log("Estrella Precision Después: " + estrellaPrecision);
+            Debug.Log("Estrella Tiempo Después: " + estrellaTiempo);
+            Debug.Log("Estrella Terminado Después: " + estrellaTerminado);
+    
+            PlayerPrefs.SetInt("noEstrellas", estrellasTotales); //Enviamos la nueva cantidad de estrellas a las PlayerPrefs
+
+            estrellasTotales = PlayerPrefs.GetInt("noEstrellas"); // Guardo para mostrar
+            Debug.Log("Estrellas Totales Después:" + estrellasTotales); // Muestro
         }
     }
 
