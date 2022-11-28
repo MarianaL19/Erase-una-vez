7f9ed7333631e01799e123d8d756c26fd6787432
Osvaldo González <gonzalezascencioosvaldo@outlook.com>
2022-11-27 18:24:20 -0600

    update: ControlPantallaVictoria
    La estrella de precisión ya la otorga, en efecto, por precisión, falta la de los tiempos.

--- Erase-una-vez/Assets/Scripts/ControlPantallaVictoria.cs
+++ Erase-una-vez/Assets/Scripts/ControlPantallaVictoria.cs
@@ -16,14 +16,34 @@ public class ControlPantallaVictoria : MonoBehaviour
 
     //Variable para identificar nivel
     public int numNivel;
+
     public bool esAudio;
+
+    //Para Precision
+    //Cantidad de aciertos por caracter, viene de script de escritura
+    int aciertos; 
+    //Precision total, calculado en script
+    public float precision;
+    //Total de caracteres, viene de base de datos
+    public int totalCaracteres;
+    
+    //Para Tiempo
+    int tiempo; 
+
+    //Para Estrellas
+    //Estrella de Precision
     public int estrellaPrecision;
+    //Estrella de Tiempo
     public int estrellaTiempo;
-    public int estrellasTotales;
+    //Estrella de Nivel terminado
     public int estrellaTerminado;
+    //Cantidad de estrellas totales en posesion del usuario
+    public int estrellasTotales;
+    
 
     public void Start()
     {
+
     }
     public void NivelFinalizado()
     {
@@ -34,9 +54,18 @@ public class ControlPantallaVictoria : MonoBehaviour
         esAudio = PlayerPrefs.GetInt("varianteAudio" + numNivel) == 1 ? true : false;
 
         //Recuperamos las estrellas del nivel
-        estrellaPrecision = PlayerPrefs.GetInt("estrellaPrecision" + numNivel); // Recupero si la estrella de precisión ya fue desbloqueada    
+        estrellaPrecision = PlayerPrefs.GetInt("estrellaPrecision" + numNivel); // Recupero si la estrella de precision ya fue desbloqueada    
         estrellaTiempo = PlayerPrefs.GetInt("estrellaTiempo" + numNivel); // Recupero si la estrella de tiempo ya fue desbloqueada
         estrellaTerminado = PlayerPrefs.GetInt("estrellaTerminado" + numNivel); // Recupero si la estrella de terminar ya fue desbloqueada
+
+        //Asignamos total de caracteres y aciertos para calcular precision
+        int aciertos = escritura.getAciertos();
+        Debug.Log("Aciertos totales: " + aciertos); // Muestro
+        totalCaracteres = PlayerPrefs.GetInt("totalCaracteres" + numNivel);
+        Debug.Log("Caracteres Totales: " + totalCaracteres); // Muestro
+
+        //Asignamos tiempo
+        int tiempo = escritura.getTiempo();
 
         //Logs para ver si las estrellas ya estaban desde antes
         Debug.Log("Estrella Precision Antes: " + estrellaPrecision);
@@ -46,11 +75,11 @@ public class ControlPantallaVictoria : MonoBehaviour
         estrellasTotales = PlayerPrefs.GetInt("noEstrellas"); // Recupero la cantidad total de estrellas
         Debug.Log("Estrellas Totales Antes:" + estrellasTotales); // Muestro la cantidad total de estrellas antes del nivel
         
-
         //Guardamos la estrella por completar el nivel
         PlayerPrefs.SetInt("estrellaTerminado" + numNivel, 1);
 
         int estrellasMostrar= 1;
+
         //PlayerPrefs.GetInt("varianteAudio") == 1;
         if(esAudio)
         {
@@ -59,7 +88,6 @@ public class ControlPantallaVictoria : MonoBehaviour
             estrella1.SetActive(false);
             estrella2.SetActive(false);
             estrella3.SetActive(false);
-
         }
         else
         {
@@ -69,13 +97,15 @@ public class ControlPantallaVictoria : MonoBehaviour
             estrellas.SetActive(true);
             estrella1.SetActive(true);
 
-            int tiempo = escritura.getTiempo();
-            int aciertos = escritura.getAciertos();
-         
-            if (tiempo < 10){
+            //Calculamos precisión
+            precision = (100.0f / totalCaracteres) * aciertos;
+            Debug.Log("Precisión total: " + precision);
 
-                if(estrellaTiempo == 0){
-                    estrellasTotales++;
+            if (tiempo < 300){
+
+                //Validación para no dar la estrella de nuevo
+                if(estrellaTiempo == 0){ 
+                    estrellasTotales++; 
                 }
 
                 estrellasMostrar++;
@@ -83,8 +113,9 @@ public class ControlPantallaVictoria : MonoBehaviour
                 estrellaTiempo = PlayerPrefs.GetInt("estrellaTiempo" + numNivel); // Marco la estrella de tiempo como desbloqueada
             }
                 
-            if (aciertos > 500) {
+            if (precision > 80) {
 
+                //Validación para no dar la estrella de nuevo
                 if(estrellaPrecision == 0){
                     estrellasTotales++;
                 }
@@ -94,7 +125,8 @@ public class ControlPantallaVictoria : MonoBehaviour
                 estrellaPrecision = PlayerPrefs.GetInt("estrellaPrecision" + numNivel); // Marco la estrella de precision como desbloqueada    
             }
 
-            if (estrellaTerminado == 0) {
+            //Validación para no dar la estrella de nuevo
+            if (estrellaTerminado == 0) { 
                 estrellasTotales++; 
             }
                 
@@ -131,4 +163,5 @@ public class ControlPantallaVictoria : MonoBehaviour
     {
         SceneManager.LoadScene("Nivel");
     }
+
 }
